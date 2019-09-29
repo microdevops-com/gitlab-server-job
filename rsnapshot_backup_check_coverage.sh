787c9b27f873b9f29e9ed1693bc0ac708887237e
Eugene Antonov <jetman@sysadm.ws>
2019-09-29 12:14:39 +0300

    check_coverage fix

--- rsnapshot_backup_check_coverage.sh
+++ rsnapshot_backup_check_coverage.sh
@@ -4,7 +4,7 @@ set -e
 # Run rsnapshot_backup_check_coverage.sh only if minion connected to this master, no error if not connected to this master
 if cat alive_minions/${CI_RUNNER_DESCRIPTION}_${SALT_MINION} | grep -q 1; then
 	echo One connected needed Minion found, running rsnapshot_backup_check_coverage.sh on this Salt Master
-	sudo /srv/scripts/ci_sudo/rsnapshot_backup_check_coverage.sh "${SALT_TIMEOUT}" "${SALT_MINION}" "${RSNAPSHOT_BACKUP_TYPE}"
+	sudo /srv/scripts/ci_sudo/rsnapshot_backup_check_coverage.sh "${SALT_MINION}"
 else
 	echo Skipping this Salt Master
 fi
