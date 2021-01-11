ab5ae9fd73eb63b9dc4aa58a547fb83813ba5fe9
Eugene Antonov <jetman@sysadm.ws>
2021-01-11 15:44:18 +0200

    prune tags via git added

--- prune_run_tags.sh
+++ prune_run_tags.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Check vars
-if [ "_$1" = "_" -o "_$2" = "_" ]; then
-	echo ERROR: needed args missing: use prune_run_tags.sh SALT_PROJECT TAGS_KEEP_AGE
+if [ "_$1" = "_" -o "_$2" = "_" -o "_$3" = "_" ]; then
+	echo ERROR: needed args missing: use prune_run_tags.sh SALT_PROJECT TAGS_KEEP_AGE api/git
 	exit 1
 fi
 if [ "_${GL_ADMIN_PRIVATE_TOKEN}" = "_" ]; then
@@ -17,12 +17,15 @@ fi
 
 SALT_PROJECT=$1
 TAGS_KEEP_AGE=$2
+METHOD=$3
+
+PROJECTS_SUBDIR=tmp/projects
 
 # Temp file for headers
 HEADERS_FILE=$(mktemp)
 
 # Exit if lock exists (prevent multiple execution)
-LOCK_DIR=prune_run_tags.lock
+LOCK_DIR=.locks/prune_run_tags.lock
 
 function restore_protection () {
 	echo NOTICE: restoring protection for 'run_*' tags:
@@ -31,10 +34,16 @@ function restore_protection () {
 	echo
 }
 
+function clean_projects_subdir () {
+	echo NOTICE: cleaning ${PROJECTS_SUBDIR}:
+	rm -rf ${PROJECTS_SUBDIR}
+	echo
+}
+
 if mkdir "${LOCK_DIR}"
 then
 	echo -e >&2 "NOTICE: Successfully acquired lock on ${LOCK_DIR}"
-	trap 'rm -rf "${LOCK_DIR}"; rm -f ${HEADERS_FILE}; restore_protection' 0
+	trap 'rm -rf "${LOCK_DIR}"; rm -f ${HEADERS_FILE}; restore_protection; clean_projects_subdir' 0
 else
 	echo -e >&2 "ERROR: Cannot acquire lock, giving up on ${LOCK_DIR}"
 	exit 1
@@ -61,51 +70,88 @@ curl -sS -X DELETE -H "PRIVATE-TOKEN: ${GL_ADMIN_PRIVATE_TOKEN}" \
 	"${GL_URL}/api/v4/projects/${GITLAB_PROJECT_ID}/protected_tags/run_*"
 echo
 
-echo NOTICE: prunning old 'run_*' tags:
+if [ "${METHOD}" = "api" ]; then
+	echo NOTICE: prunning old 'run_*' tags via api:
 
-# Initial page
-PAGE_LINK="${GL_URL}/api/v4/projects/${GITLAB_PROJECT_ID}/repository/tags?pagination=keyset&per_page=50&sort=asc&search=^run_"
-echo NOTICE: first page link: ${PAGE_LINK}
+	# Initial page
+	PAGE_LINK="${GL_URL}/api/v4/projects/${GITLAB_PROJECT_ID}/repository/tags?pagination=keyset&per_page=50&sort=asc&search=^run_"
+	echo NOTICE: first page link: ${PAGE_LINK}
 
-# Loop while link not empty
-while [[ -n ${PAGE_LINK} ]]; do
-	TAGS_PAGE=$(curl -sS -D ${HEADERS_FILE} -X GET -H "PRIVATE-TOKEN: ${GL_ADMIN_PRIVATE_TOKEN}" \
-		-H "Content-Type: application/json" \
-		${PAGE_LINK} | jq -c ".[]")
+	# Loop while link not empty
+	while [[ -n ${PAGE_LINK} ]]; do
+		TAGS_PAGE=$(curl -sS -D ${HEADERS_FILE} -X GET -H "PRIVATE-TOKEN: ${GL_ADMIN_PRIVATE_TOKEN}" \
+			-H "Content-Type: application/json" \
+			${PAGE_LINK} | jq -c ".[]")
 
-	# Pagination skips tags between pages if we delete tags, so we will retry the same page if there were deletinions
-	NEED_RETRY=false
+		# Pagination skips tags between pages if we delete tags, so we will retry the same page if there were deletinions
+		NEED_RETRY=false
 
-	# Loop for tags on page
+		# Loop for tags on page
+		IFS=$'\n'
+		for TAGS_ROW in ${TAGS_PAGE}; do
+			echo NOTICE: ---
+			TAG_NAME=$(echo ${TAGS_ROW} | jq -r '.name')
+			echo NOTICE: tag name: ${TAG_NAME}
+			TAG_DATE=$(echo ${TAG_NAME} | sed -r 's/^run_.+([0-9]{4}-[0-9]{2}-[0-9]{2})_[0-9]{2}-[0-9]{2}-[0-9]{2}$/\1/')
+			echo NOTICE: date from tag name: ${TAG_DATE}
+			TAG_AGE=$(( (`date -d "00:00" +%s` - `date -d "${TAG_DATE}" +%s`) / (24*3600) ))
+			echo NOTICE: tag age: ${TAG_AGE}
+
+			# Delete tag if older than age
+			if [[ ${TAG_AGE} -ge ${TAGS_KEEP_AGE} ]]; then
+				echo NOTICE: tag age ${TAG_AGE} is greater or equal ${TAGS_KEEP_AGE}, deleting
+				curl -sS -X DELETE -H "PRIVATE-TOKEN: ${GL_ADMIN_PRIVATE_TOKEN}" \
+					"${GL_URL}/api/v4/projects/${GITLAB_PROJECT_ID}/repository/tags/${TAG_NAME}"
+				
+				# We need retry only with deletes
+				NEED_RETRY=true
+			fi
+		done
+		
+		if [[ ${NEED_RETRY} = true ]]; then
+			# No changes in the link
+			echo NOTICE: next page link with retry: ${PAGE_LINK}
+		else
+			# Take next page link from response headers
+			PAGE_LINK=$(cat ${HEADERS_FILE} | grep -i '^Link:.*; rel="next"' | sed -r 's/^.*<(https:.+)>; rel="next".*$/\1/')
+			echo NOTICE: next page link: ${PAGE_LINK}
+		fi
+	done
+elif [ "${METHOD}" = "git" ]; then
+	echo NOTICE: prunning old 'run_*' tags via git:
+
+	# Make repo dir 
+	mkdir -p ${PROJECTS_SUBDIR}/${SALT_PROJECT}
+
+	# Clone repo
+	GL_URL_WITHOUT_HTTPS=$(echo ${GL_URL} | sed -e 's#https://##')
+	git clone https://root:${GL_ADMIN_PRIVATE_TOKEN}@${GL_URL_WITHOUT_HTTPS}/${SALT_PROJECT}.git ${PROJECTS_SUBDIR}/${SALT_PROJECT}
+
+	# Loop tags
 	IFS=$'\n'
-	for TAGS_ROW in ${TAGS_PAGE}; do
+	for TAGS_ROW in $(cd ${PROJECTS_SUBDIR}/${SALT_PROJECT} && git tag -l); do
 		echo NOTICE: ---
-		TAG_NAME=$(echo ${TAGS_ROW} | jq -r '.name')
+		TAG_NAME=${TAGS_ROW}
 		echo NOTICE: tag name: ${TAG_NAME}
 		TAG_DATE=$(echo ${TAG_NAME} | sed -r 's/^run_.+([0-9]{4}-[0-9]{2}-[0-9]{2})_[0-9]{2}-[0-9]{2}-[0-9]{2}$/\1/')
 		echo NOTICE: date from tag name: ${TAG_DATE}
 		TAG_AGE=$(( (`date -d "00:00" +%s` - `date -d "${TAG_DATE}" +%s`) / (24*3600) ))
 		echo NOTICE: tag age: ${TAG_AGE}
 
-		# Delete tag if older than age
+		# Delete local tag if older than age
 		if [[ ${TAG_AGE} -ge ${TAGS_KEEP_AGE} ]]; then
 			echo NOTICE: tag age ${TAG_AGE} is greater or equal ${TAGS_KEEP_AGE}, deleting
-			curl -sS -X DELETE -H "PRIVATE-TOKEN: ${GL_ADMIN_PRIVATE_TOKEN}" \
-				"${GL_URL}/api/v4/projects/${GITLAB_PROJECT_ID}/repository/tags/${TAG_NAME}"
-			
-			# We need retry only with deletes
-			NEED_RETRY=true
+			# In a subshell to keep current working dir
+			( cd ${PROJECTS_SUBDIR}/${SALT_PROJECT} && git tag -d ${TAG_NAME} )
 		fi
 	done
-	
-	if [[ ${NEED_RETRY} = true ]]; then
-		# No changes in the link
-		echo NOTICE: next page link with retry: ${PAGE_LINK}
-	else
-		# Take next page link from response headers
-		PAGE_LINK=$(cat ${HEADERS_FILE} | grep -i '^Link:.*; rel="next"' | sed -r 's/^.*<(https:.+)>; rel="next".*$/\1/')
-		echo NOTICE: next page link: ${PAGE_LINK}
-	fi
-done
+
+	# Push deleted tags to remote
+	( cd ${PROJECTS_SUBDIR}/${SALT_PROJECT} && git push --tags --prune )
+else
+	echo ERROR: method is not api or git
+	exit 1
+fi
 
 restore_protection
+clean_projects_subdir
