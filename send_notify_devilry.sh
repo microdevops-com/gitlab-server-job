beae2262895c811b9a2b78c59445c11c933b1ab7
Eugene Antonov <jetman@sysadm.ws>
2021-02-04 20:25:52 +0200

    send_notify_devilry.sh

--- send_notify_devilry.sh
+++ send_notify_devilry.sh
@@ -4,7 +4,7 @@ set -e
 # Run send_notify_devilry.sh only if minion connected to this master, no error if not connected to this master
 if cat alive_minions/${CI_RUNNER_DESCRIPTION}_${SALT_MINION} | grep -q 1; then
 	echo One connected needed Minion found, running send_notify_devilry.sh on this Salt Master
-	sudo /srv/scripts/ci_sudo/send_notify_devilry.sh
+	sudo --preserve-env /srv/scripts/ci_sudo/send_notify_devilry.sh
 else
 	echo Skipping this Salt Master
 fi
